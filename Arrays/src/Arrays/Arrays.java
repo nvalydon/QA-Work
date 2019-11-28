79e3878690e83612ae523d22293a82a53395b037
nvalydon <58259202+nvalydon@users.noreply.github.com>
2019-11-28 11:39:58 +0000

    Add files via upload

---
+++ Arrays/src/Arrays/Arrays.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+	
+		
+	
+		
+		
+	int [] numArray = new int[10];
+	
+	
+	for(int i = 0; i < numArray.length; i++) {
+	
+		numArray[i] = i+1; 
+		
+		
+	}
+	
+	for(int j = 0; j < numArray.length; j++) {
+		
+		System.out.print("\t" + numArray[j]);
+		System.out.println("\t" + numArray[j]*10);
+		
+		
+	}
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+	 
+	
+	
+	
+	
+}
